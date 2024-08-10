d10eb7a9c4fabd89f9abf2dc61fe8388b2c89e8d
Petra <petra@behemot.biz>
2024-08-10 10:13:11 +0200

    Add custom templates

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,5 @@ class PostList(generic.ListView):
     # queryset = Post.objects.all().order_by("-created_on")
     # queryset = Post.objects.filter(author=2)
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
